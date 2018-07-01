0514ddcd2e59e49e8df0b20cf2f5c8b05a192043
Anuarder <anuar.ibraev97@gmail.com>
2018-07-02 00:10:47 +0600

    Added main content

--- src/js/main.js
+++ src/js/main.js
@@ -1,120 +1,131 @@
 
-
 let miData = [
 	{
-		"Модель":"Xiaomi MI Mix 2",
-		"Вес":"185г",
-		"Процессор":"Qualcomm Snapdragon 835",
-		"Тип экрана":"IPS LCD",
-		"Размер экрана":"5,99",
-		"Разрешение экрана":"2160x1080",
-		"Видеопроцессор":"Adreno 540",
-		"Фронтальная камера":"5 МП",
-		"Объем оперативной памяти":"6 Гб",
-		"Объем встроенной памяти":"64 Гб",
-		"Тыловая фотокамера":"12 МП",
-		"Диафрагма":"F/2.2",
-		"Макс. разрешение видео":"3840x2160 (4K)",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
-		"count":"6",
-		"price":"200.000тг",
-		"file":"./media/Mix2.jpg"
+		"id":"0",
+		"Model":"Xiaomi MI Mix 2",
+		"Weight":"185г",
+		"CPU":"Qualcomm Snapdragon 835",
+		"Screen-type":"IPS LCD",
+		"Screen-size":"5,99",
+		"Screen-resolution":"2160x1080",
+		"GPU":"Adreno 540",
+		"RAM":"6 Гб",
+		"ROM":"64 Гб",
+		"Camera":{
+			"Primary":"12 МП, F/2.0",
+			"Secondary":"5 МП"
+		},
+		"Video":"3840x2160 (4K)",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"Count":"2",
+		"Price":"200.000тг",
+		"File":"./media/Mix2.jpg"
 	},
 	{
-		"Модель":"Xiaomi MI Note 2",
-		"Вес":"166г",
-		"Процессор":"Qualcomm Snapdragon 821",
-		"Тип экрана":"OLED",
-		"Размер экрана":"5,7",
-		"Разрешение экрана":"1920*1080",
-		"Видеопроцессор":"Adreno 530",
-		"Фронтальная камера":"8 МП",
-		"Объем оперативной памяти":"6 Гб",
-		"Объем встроенной памяти":"128 Гб",
-		"Тыловая фотокамера":"22 МП",
-		"Диафрагма":"F/2",
-		"Макс. разрешение видео":"3840x2160 (4K)",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"id":"1",
+		"Model":"Xiaomi MI Note 2",
+		"Weight":"166г",
+		"CPU":"Qualcomm Snapdragon 821",
+		"Screen-type":"OLED",
+		"Screen-size":"5,7",
+		"Screen-resolution":"1920*1080",
+		"GPU":"Adreno 530",
+		"RAM":"6 Гб",
+		"ROM":"128 Гб",
+		"Camera":{
+			"Primary":"22 МП, F/2.0",
+			"Secondary":"8 МП"
+		},
+		"Video":"3840x2160 (4K)",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
 		"count":"8",
-		"price":"132.000тг",
-		"file":"./media/MiNote2.jpg"
+		"Price":"132.000тг",
+		"File":"./media/MiNote2.jpg"
 	},
 	{
-		"Модель":"Redmi Note 5",
-		"Вес":"181г",
-		"Процессор":"Qualcomm Snapdragon 836",
-		"Тип экрана":"IPS",
-		"Размер экрана":"5,99",
-		"Разрешение экрана":"2160х1080",
-		"Видеопроцессор":"Adreno 509",
-		"Фронтальная камера":"13 МП",
-		"Объем оперативной памяти":"6 Гб",
-		"Объем встроенной памяти":"128 Гб",
-		"Тыловая фотокамера":"12+5 МП",
-		"Диафрагма":"F/2",
-		"Макс. разрешение видео":"3840x2160 (4K)",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"id":"2",
+		"Model":"Redmi Note 5",
+		"Weight":"181г",
+		"CPU":"Qualcomm Snapdragon 836",
+		"Screen-type":"IPS",
+		"Screen-size":"5,99",
+		"Screen-resolution":"2160х1080",
+		"GPU":"Adreno 509",
+		"RAM":"6 Гб",
+		"ROM":"128 Гб",
+		"Camera":{
+			"Primary":"12+5 МП, F/2.0",
+			"Secondary":"13 МП"
+		},
+		"Video":"3840x2160 (4K)",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
 		"count":"11",
-		"price":"93.000тг",
-		"file":"./media/MiNote5.jpg"
+		"Price":"93.000тг",
+		"File":"./media/MiNote5.jpg"
 	},
 	{
-		"Модель":"Redmi Note 4",
-		"Вес":"165г",
-		"Процессор":"Qualcomm Snapdragon 625",
-		"Тип экрана":"IPS",
-		"Размер экрана":"5,5",
-		"Разрешение экрана":"1920*1080",
-		"Видеопроцессор":"Adreno 506",
-		"Фронтальная камера":"5 МП",
-		"Объем оперативной памяти":"4 Гб",
-		"Объем встроенной памяти":"64 Гб",
-		"Тыловая фотокамера":"13 МП",
-		"Диафрагма":"F/2.2",
-		"Макс. разрешение видео":"1920*1080",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"id":"3",
+		"Model":"Redmi Note 4",
+		"Weight":"165г",
+		"CPU":"Qualcomm Snapdragon 625",
+		"Screen-type":"IPS",
+		"Screen-size":"5,5",
+		"Screen-resolution":"1920*1080",
+		"GPU":"Adreno 506",
+		"RAM":"4 Гб",
+		"ROM":"64 Гб",
+		"Camera":{
+			"Primary":"13 МП, F/2.2",
+			"Secondary":"5 МП"
+		},
+		"Video":"1920*1080",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
 		"count":"3",
-		"price":"62.000тг",
-		"file":"./media/MiNote4.jpg"
+		"Price":"62.000тг",
+		"File":"./media/MiNote4.jpg"
 	},
 	{
-		"Модель":"Xiaomi Mi A1",
-		"Вес":"165г",
-		"Процессор":"Qualcomm Snapdragon 625",
-		"Тип экрана":"IPS",
-		"Размер экрана":"5,5",
-		"Разрешение экрана":"1920*1080",
-		"Видеопроцессор":"Adreno 506",
-		"Фронтальная камера":"5 МП",
-		"Объем оперативной памяти":"4 Гб",
-		"Объем встроенной памяти":"64 Гб",
-		"Тыловая фотокамера":"12+12 МП",
-		"Диафрагма":"F/2.2",
-		"Макс. разрешение видео":"3840x2160 (4K)",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"id":"4",
+		"Model":"Xiaomi Mi A1",
+		"Weight":"165г",
+		"CPU":"Qualcomm Snapdragon 625",
+		"Screen-type":"IPS",
+		"Screen-size":"5,5",
+		"Screen-resolution":"1920*1080",
+		"GPU":"Adreno 506",
+		"RAM":"4 Гб",
+		"ROM":"64 Гб",
+		"Camera":{
+			"Primary":"12+12 МП, F/2.2",
+			"Secondary":"5 МП"
+		},
+		"Video":"3840x2160 (4K)",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
 		"count":"5",
-		"price":"68.000тг",
-		"file":"./media/MiA1.jpg"
+		"Price":"68.000тг",
+		"File":"./media/MiA1.jpg"
 	},
 	{
-		"Модель":"Xiaomi Redmi 5 Plus",
-		"Вес":"180г",
-		"Процессор":"Qualcomm Snapdragon 625",
-		"Тип экрана":"IPS",
-		"Размер экрана":"5,99",
-		"Разрешение экрана":"2160х1080",
-		"Видеопроцессор":"Adreno 506",
-		"Фронтальная камера":"5 МП",
-		"Объем оперативной памяти":"4 Гб",
-		"Объем встроенной памяти":"64 Гб",
-		"Тыловая фотокамера":"12 МП",
-		"Диафрагма":"F/2.2",
-		"Макс. разрешение видео":"1920*1080",
-		"Интерфейсы":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
+		"id":"5",
+		"Model":"Xiaomi Redmi 5 Plus",
+		"Weight":"180г",
+		"CPU":"Qualcomm Snapdragon 625",
+		"Screen-type":"IPS",
+		"Screen-size":"5,99",
+		"Screen-resolution":"2160х1080",
+		"GPU":"Adreno 506",
+		"RAM":"4 Гб",
+		"ROM":"64 Гб",
+		"Camera":{
+			"Primary":"12 МП, F/2.2",
+			"Secondary":"5 МП"
+		},
+		"Video":"1920*1080",
+		"COMMS":"Wi-Fi 802.11ac, Wi-Fi Direct, Bluetooth 5.0, IRDA, USB, NFC",
 		"count":"13",
-		"price":"77.000тг",
-		"file":"./media/MiPlus5.jpg"
-	}
+		"Price":"77.000тг",
+		"File":"./media/MiPlus5.jpg"
+	},
 ];
 
 let vm = new Vue({
@@ -128,7 +139,7 @@ let vm = new Vue({
 		shoppingCart:{
 			count: 2,
 			items: '',
-			price: 54524
+			Price: 54524
 		}
 	},
 	methods:{
@@ -137,6 +148,6 @@ let vm = new Vue({
 		},
 		viewActiveToggle(id){
 			this.classes.viewActive = id;
-		}
+		},
 	}
 });
